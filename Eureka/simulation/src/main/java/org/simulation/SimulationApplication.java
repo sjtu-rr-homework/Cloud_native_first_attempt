0b33e990c839374d5689d6d71ec8295da12605dc
macoredroid <coredroid0401@gmail.com>
2019-06-22 20:56:58 +0800

    add hystrix

--- Eureka/simulation/src/main/java/org/simulation/SimulationApplication.java
+++ Eureka/simulation/src/main/java/org/simulation/SimulationApplication.java
@@ -3,12 +3,17 @@ package org.simulation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableTurbine
 @RestController
 public class SimulationApplication {
 
